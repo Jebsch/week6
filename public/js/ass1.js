660a07e52170f888873ffdbcfd381bab7dbe103d
Jebsch <124074543+Jebsch@users.noreply.github.com>
2023-02-22 02:24:59 +0000

    done?

--- public/js/ass1.js
+++ public/js/ass1.js
@@ -10,21 +10,6 @@ showdate();
 }, 1000);
 
 
-function changetab (evt, tabname){
-  var i, tabs, tablinks;
-
-  tabs=document.getElementsByClassName("tabs");
-  
-    tabs[1].style.display = "none";
-
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < 1; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(tabname).style.display = "block";
-  evt.currentTarget.className += " active";
-}  
-
 function validatedashboard() {
   let x = document.forms["Form"]["fname"].value;
   if (x == "") {
@@ -34,40 +19,18 @@ function validatedashboard() {
 }
 
 function validateprofile() {
-  let first = document.forms["myForm"]["Firstname"].value;
-  let last = document.forms["myForm"]["Lastname"].value;
-  let full = Firstname + " " + Lastname
+  let first = document.forms["profileform"]["Firstname"].value;
+  let last = document.forms["profileform"]["Lastname"].value;
+  let full = Firstname + " " + Lastname;
+  let email = document.forms["profileform"]["Emailinput"].value;
+  $("email").text(email);
   $("Fullname").text(full);
+
    }
-
-
-const dispenseMoney = (evt) => {
-
-  evt.preventDefault();  
-  
-  let money = parseInt($("#money").value);
-
-  let isPossible = false;
-
-  if (money >= 5 && money % 5 == 0) {
-    isPossible = true;
-  }
-
-  if (isPossible) {
-    let moneyLeftToDispense = money;
-
-    for (let bill of bills) {
-      if (moneyLeftToDispense >= bill) {
-        let amountBills = parseInt(moneyLeftToDispense / bill);
-
-        moneyLeftToDispense = moneyLeftToDispense % bill;
-        $(`#bill${bill}s`).value = amountBills;
-      }
-    }
-  } else {
-    $("#money-error").textContent = "We can't dispense that amount of money";
-  }
-};
+first.onchange = validateprofile;
+last.onchange = validateprofile;
+full.onchange = validateprofile;
+email.onchange = validateprofile;
 
 var firstname = document.getElementById('firstname').value;
 var lastname = document.getElementById('lastname').value;
@@ -78,45 +41,26 @@ document.getElementById("ProfileForm").submit() = (evt) => {
   evt.preventDefault(); 
   
 };
+   var password = document.getElementById("password");
+   var confirm_password = document.getElementById("confirm_password");
+ 
+ function validatePassword(event, inputText){
+  event.preventDefault
+   if(password.value != confirm_password.value) {
+     confirm_password.setCustomValidity("Passwords Don't Match");
+   } else {
+     confirm_password.setCustomValidity('');
+   }
+ }
+ 
+ password.onchange = validatePassword;
+ confirm_password.onkeyup = validatePassword;
 
+ var profileform = document.getElementById("retForm");
+ var retOp = document.getElementById("retOp");
+ function submitFormReturn(event) {
+  profileform.style.display = "none";
+    retOp.innerHTML = "<b>Form submit successful</b>";
+ }
+ 
 
-const validateTemp = (evt) =>{
-  let temps = parseFloat($("#temps").value)
-  if((temps%.5)==0){
-    $("temp-error").textcontent="";
-  }else{
-    $("temp-error").textcontent="Temperature should be devisible by 0.5";
-  }
-
- // let re = new RegExp("^\[0-9]{10}")
-
- // let phonenumber = evt.currentTarget.value;
-
- // if(re.test(phonenumber)){
- //   $("phone-number-error").textcontent = "";
- // }else{
- //   $("phone-number-error").textcontent = "The phone number should be in formal ##########";  
- //   }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-
-  $("#reset").addEventListener("click", () => {
-    $("#money").value = "";
-    $("#money-error").textContent = "";
-
-    for (let bill of bills) {
-      $(`#bill${bill}s`).value = "";
-    }
-
-    $("#money").focus();
-  });
-
-  $("#phone-number").addEventListener("input". validatePhoneNumber);
-
-  $("#dispense-button").addEventListener("click", dispenseMoney);
-
-  $("#money").value = 60;
-
-  $("#money").focus();
-});
